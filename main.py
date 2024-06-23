7e1da97d3c675e43726ec2fbbcc6032dab6a12f6
Jonas Heilig <jonas.maximilian@die-heiligs.de>
2024-06-23 11:52:08 +0200

    Create Project Structure Start Writing main.py Try Bootstrap Studio

--- main.py
+++ main.py
@@ -13,10 +13,15 @@ db = SQLAlchemy(app)
 
 class ProjectList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(db.Integer, nullable=False)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    isadmin = db.Column(db.Boolean, default=False)
+    isauthor = db.Column(db.Boolean, default=False)
 
 
 class Posts(db.Model):
@@ -32,6 +37,51 @@ if not os.path.exists('instance/db.db'):
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/aboutme')
+def aboutme():
+    pass
+
+
+@app.route('/blog')
+def blog():
+    pass
+
+
+@app.route('/projects')
+def projects():
+    pass
+
+
+@app.route('/project/<int:project_id>')
+def project(project_id):
+    pass
+
+
+@app.route('/download/<int:project_id>')
+def download(project_id):
+    pass
+
+
+@app.route('/admin')
+def admin():
+    pass
+
+
+@app.route('/write_blog')
+def write_article():
+    pass
+
+
+@app.route('/add_project')
+def add_project():
+    pass
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    pass
 
 
 if __name__ == '__main__':
